9af8a84f594025f3aa6023caa43e3ed2d217566e
calebburb <54636139+calebburb@users.noreply.github.com>
2020-09-11 15:53:27 -0500

    submitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './css/App.css'
+import Card from './components/Card.js'
+import Confetti from './components/Confetti.js'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo'>C A L E B </div>
+
+      <div className='top-half'>
+        <img
+          className='profilePic'
+          src={require('./assets/profilePic-removebg-preview.png')}
+        />
+
+        <h1>
+          Hello, I'm{' '}
+          <a className='name' href='https://www.instagram.com/ui.burb/'>
+            Caleb
+          </a>{' '}
+          Burbach ✌️ <br></br> Welcome to my portfolio
+        </h1>
+      </div>
+      <Confetti></Confetti>
+      <div className='cards'>
+        <Card title='Parkd' id='ParkdCard'></Card>
+        <Card title='UNL Food Delivery' id='UNLFDCard'></Card>
+        <Card title='Frat Payment Web-App' id='FratPaymentCard'></Card>
+        <Card title='Sonic App Redesign' id='SonicCard'></Card>
+        <Card title='UNL Baseball App' id='BaseballCard'></Card>
+        <Card title='Wail Mail' id='WailMailCard'></Card>
+        <Card title='Wikipedia Redesign' id='WikipediaCard'></Card>
+        <Card title='Spotify Redesign' id='SpotifyCard'></Card>
+      </div>
+      <div className='footer'></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import '../css/App.css'
+
+class Card extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      expandedCard: false
+    }
+    this.toggleExpandedCard = this.toggleExpandedCard.bind(this)
+  }
+
+  toggleExpandedCard () {
+    this.setState({ expandedCard: !this.state.expandedCard })
+  }
+
+  render () {
+    return (
+      <div
+        className='card'
+        className={`${this.state.expandedCard ? 'expanded-card' : 'card'}`}
+        id={this.props.id}
+        onClick={() => {
+          this.toggleExpandedCard()
+        }}
+      >
+        <a href={'#' + this.props.id}>
+          <h1>{this.props.title}</h1>
+        </a>
+
+      </div>
+    )
+  }
+}
+
+export default Card
