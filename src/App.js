a7e853f80b9627d4009d89c8d3a3a32061b30b6c
calebburb <54636139+calebburb@users.noreply.github.com>
2021-04-01 17:58:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './css/App.css'
 import Card from './components/Card.js'
 import Confetti from './components/Confetti.js'
+document.write( '<div class=\"embedsocial-hashtag\" data-ref=\"a9f1679cabfe621ebd8dd0ed6fe52de3448ba29a\" ><a class=\"feed-powered-by-es\" href=\"https://embedsocial.com/products/embedfeed/\" target=\"_blank\" title=\"Powered by EmbedSocial\">Powered by EmbedSocial<span>→</span></a></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = \"https://embedsocial.com/cdn/ht.js\"; d.getElementsByTagName(\"head\")[0].appendChild(js);}(document, \"script\", \"EmbedSocialHashtagScript\"));</script>' );
 
 function App () {
   return (
@@ -22,7 +23,7 @@ function App () {
         </h1>
       </div>
       <Confetti></Confetti>
-      <div className='cards'>
+      {/* <div className='cards'>
         <Card title='Parkd' id='ParkdCard'></Card>
         <Card title='UNL Food Delivery' id='UNLFDCard'></Card>
         <Card title='Frat Payment Web-App' id='FratPaymentCard'></Card>
@@ -31,9 +32,8 @@ function App () {
         <Card title='Wail Mail' id='WailMailCard'></Card>
         <Card title='Wikipedia Redesign' id='WikipediaCard'></Card>
         <Card title='Spotify Redesign' id='SpotifyCard'></Card>
-      </div>
 
-      <div className='footer'></div>
+      </div> */}
     </div>
   )
 }
